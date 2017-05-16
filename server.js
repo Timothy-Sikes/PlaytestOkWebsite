6159f984d619c6527e7ed9cfd1c5f9e4c83895cb
Timothy Sikes <tsikes64@gmail.com>
2017-05-16 13:29:47 -0500

    Set port number by environment variable.

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var finalhandler = require('finalhandler');
 var serverStatic = require('serve-static');
 
 var serve = serverStatic('./content/static');
+var port =  (process.env.PORT || 5000);
+
 
 var server = http.createServer(function(req, res) {
     var done = finalhandler(req, res);
     serve(req, res, done);
 })
 
-server.listen(8000);
+server.listen(port);
