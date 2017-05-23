15583b3304987c93914da6498526a5f62ba1058c
Timothy Sikes <tsikes64@gmail.com>
2017-05-23 11:33:36 -0500

    Rename old file to main.html.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static('./content/static/'));
 
 app.get('/', function (req, res) {
-  res.sendfile('./content/static/HTML/mainNew.html');
+  res.sendfile('./content/static/HTML/main.html');
 })
 
 app.listen((process.env.PORT || 8000));
